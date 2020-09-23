486ff0c0df8b6276a69cecdabaa3cbd6932523d8
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-22 19:13:16 -0500

    commit the header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,14 @@
 //import react and its hooks
 import React, { useState } from 'react';
 //import components
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import {
+	AppBar,
+	Toolbar,
+	Typography,
+	IconButton,
+	Menu,
+	MenuItem,
+} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 //import use styles
 import { useStyles } from './styles';
@@ -14,24 +17,61 @@ import { useStyles } from './styles';
 export const Header = () => {
 	//use styles
 	const classes = useStyles();
+	//use state hook
+	const [anchorEl, setAnchorEl] = useState(null);
+
+	//handler the click of the menu button
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	//handle the close of the menu button
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
 	return (
-		<div className={classes.root}>
-			<AppBar position='static'>
-				<Toolbar>
-					<IconButton
-						edge='start'
-						className={classes.menuButton}
-						color='inherit'
-						aria-label='menu'>
-						<MenuIcon />
-					</IconButton>
-					<Typography variant='h6' className={classes.title}>
-						News
-					</Typography>
-					<Button color='inherit'>Login</Button>
-				</Toolbar>
-			</AppBar>
-		</div>
+		<>
+			{/*App bar component from material ui*/}
+			<div className={classes.root}>
+				{/*App bar component*/}
+				<AppBar aria-label='nav bar' position='static'>
+					{/*tool bar component from material ui*/}
+					<Toolbar aria-label='tool bar'>
+						<IconButton
+							onClick={handleClick}
+							edge='start'
+							className={classes.menuButton}
+							color='inherit'
+							aria-label='menu button'>
+							<MenuIcon />
+						</IconButton>
+						<Menu
+							aria-label='menu'
+							id='simple-menu'
+							anchorEl={anchorEl}
+							keepMounted
+							open={Boolean(anchorEl)}
+							onClose={handleClose}>
+							<MenuItem aria-label='menu item' onClick={handleClose}>
+								Profile
+							</MenuItem>
+							<MenuItem aria-label='menu item' onClick={handleClose}>
+								My account
+							</MenuItem>
+							<MenuItem aria-label='menu item' onClick={handleClose}>
+								Logout
+							</MenuItem>
+						</Menu>
+						<Typography
+							align='center'
+							aria-label='title'
+							variant='h6'
+							className={classes.title}>
+							User Crud
+						</Typography>
+					</Toolbar>
+				</AppBar>
+			</div>
+		</>
 	);
 };
