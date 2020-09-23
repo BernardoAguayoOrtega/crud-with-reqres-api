59afa6e71201aceb762ac1edf47b4db1d11d1da5
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-22 20:52:51 -0500

    add animations

--- src/pages/ListOfUsers/index.js
+++ src/pages/ListOfUsers/index.js
@@ -1,5 +1,5 @@
 //import react and its hooks
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 //import grid component from materia ui
 import { Grid } from '@material-ui/core';
 //use the context
@@ -8,12 +8,24 @@ import { Context } from '../../utils/context';
 import { UserCard } from '../../components/UserCard';
 //import pagination
 import { PaginationComponent } from '../../components/Pagination';
+//import spring tools
+import { useSpring, animated } from 'react-spring';
+//import the context
 //import styles
 import './styles.css';
 
 //create page list of users and export it
 export const ListOfUsers = () => {
+	//use the global context
 	const { users } = useContext(Context);
+
+	// confi for animation
+	const [state] = useState(true);
+	const { x } = useSpring({
+		from: { x: 0 },
+		x: state ? 1 : 0,
+		config: { duration: 1000 },
+	});
 
 	return (
 		<div className='list-of-posts'>
@@ -22,14 +34,26 @@ export const ListOfUsers = () => {
 				<Grid item xs={12}>
 					<Grid container justify='center' spacing={0}>
 						{users.map((user, i) => (
-							<UserCard
-								key={i}
-								email={user.email}
-								firstName={user.first_name}
-								lastName={user.last_name}
-								id={user.id}
-								avatar={user.avatar}
-							/>
+							<animated.div
+								style={{
+									opacity: x.interpolate({ range: [0, 1], output: [0.3, 1] }),
+									transform: x
+										.interpolate({
+											range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+											output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+										})
+										.interpolate((x) => `scale(${x})`),
+								}}>
+								{/*show the card*/}
+								<UserCard
+									key={i}
+									email={user.email}
+									firstName={user.first_name}
+									lastName={user.last_name}
+									id={user.id}
+									avatar={user.avatar}
+								/>
+							</animated.div>
 						))}
 					</Grid>
 				</Grid>
